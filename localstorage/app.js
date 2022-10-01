971fb5d9fe2c1a967252e52ed2eab8dff47442a0
Edy Segura <edysegura@gmail.com>
2022-10-01 11:42:34 -0300

    feat: check form data with console.log

--- localstorage/app.js
+++ localstorage/app.js
@@ -2,5 +2,10 @@ const form = document.querySelector("form");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  alert("Submitted successfully");
+  const form = event.target;
+  const formData = {
+    key: form.key.value,
+    value: form.value.value,
+  };
+  console.log({ formData });
 });
