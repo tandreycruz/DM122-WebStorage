df146aeb174a10b3e3ca271f03c2aa94bcd14183
Edy Segura <edysegura@gmail.com>
2022-10-06 08:56:18 -0300

    feat: download and save image into indexedDB

--- indexeddb/app.js
+++ indexeddb/app.js
@@ -10,23 +10,19 @@ db.on("populate", async () => {
   await db.pokemon.bulkPut([
     {
       name: "Bulbasaur",
-      picture:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      picture: await downloadAndStoreImage("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"),
     },
     {
       name: "Charmander",
-      picture:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+      picture: await downloadAndStoreImage("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png"),
     },
     {
       name: "Squirtle",
-      picture:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+      picture: await downloadAndStoreImage("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png"),
     },
     {
       name: "Pikachu",
-      picture:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+      picture: await downloadAndStoreImage("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"),
     },
   ]);
 });
@@ -53,16 +49,23 @@ document.body.innerHTML = pokeHTML;
 
 function toHTML(poke) {
   return `
-    <div>
+    <a href="#" class="card-wrapper">
       <div class="card" style="border-color: var(--grass);">
         <div class="card-id" style="color: var(--grass);">${poke.id}</div>
         <div class="card-image">
-          <img alt="${poke.name}" src="${poke.picture}">
+          <img alt="${poke.name}" src="${URL.createObjectURL(poke.picture)}">
         </div>
       </div>
       <div class="card-name" style="background-color: var(--grass);">
         ${poke.name}
       </div>
-    </div>
+    </a>
   `;
+}
+
+// Download and store an image
+async function downloadAndStoreImage(imageUrl) {
+  const response = await fetch(imageUrl);
+  const blob = await response.blob();
+  return blob
 }
