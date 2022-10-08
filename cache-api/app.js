43bfa44f120ce1d9f758b8726f136de3c5a94001
Edy Segura <edysegura@gmail.com>
2022-10-08 11:50:23 -0300

    refactor: fetch from cache and fetch from network

--- cache-api/app.js
+++ cache-api/app.js
@@ -8,15 +8,13 @@ async function addToCache(requestUrl, response) {
 async function fetchFromNetwork(requestUrl) {
   const response = await fetch(requestUrl);
   addToCache(requestUrl, response.clone());
-  const pokeData = await response.json();
-  return pokeData;
+  return response;
 }
 
 async function fetchFromCache(requestUrl) {
   const cache = await caches.open(CACHE_KEY);
   const cachedResponse = await cache.match(requestUrl);
-  const pokeData = cachedResponse?.json();
-  return pokeData;
+  return cachedResponse || null;
 }
 
 async function showPokeData() {
@@ -24,8 +22,9 @@ async function showPokeData() {
   const details = document.querySelector("details");
   const pre = document.querySelector("pre");
   const requestUrl = "https://pokeapi.co/api/v2/pokemon/282";
-  const pokeData =
+  const pokeDataResponse =
     (await fetchFromCache(requestUrl)) || (await fetchFromNetwork(requestUrl));
+  const pokeData = await pokeDataResponse.json();
   pre.textContent = JSON.stringify(pokeData, null, 2);
   img.src = pokeData.sprites.other["official-artwork"].front_default;
   img.alt = pokeData.name;
