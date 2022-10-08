a4d42cb26d891351ceeb35826558d5ec8328e0e0
Edy Segura <edysegura@gmail.com>
2022-10-08 12:06:09 -0300

    feat: cache image

--- cache-api/app.js
+++ cache-api/app.js
@@ -21,13 +21,21 @@ async function showPokeData() {
   const img = document.querySelector("img");
   const details = document.querySelector("details");
   const pre = document.querySelector("pre");
-  const requestUrl = "https://pokeapi.co/api/v2/pokemon/282";
+
+  const pokeNumber = Math.floor(Math.random() * 10) + 1;
+  console.log('PokeNumber: ' + pokeNumber);
+  const requestUrl = `https://pokeapi.co/api/v2/pokemon/${pokeNumber}`;
   const pokeDataResponse =
     (await fetchFromCache(requestUrl)) || (await fetchFromNetwork(requestUrl));
   const pokeData = await pokeDataResponse.json();
   pre.textContent = JSON.stringify(pokeData, null, 2);
-  img.src = pokeData.sprites.other["official-artwork"].front_default;
   img.alt = pokeData.name;
   img.title = pokeData.name;
   details.hidden = false;
+
+  const pokeImgUrl = pokeData.sprites.other["official-artwork"].front_default;
+  const pokeImageRequest =
+  (await fetchFromCache(pokeImgUrl)) || (await fetchFromNetwork(pokeImgUrl));
+  const blob = await pokeImageRequest.blob();
+  img.src = URL.createObjectURL(blob);
 }
