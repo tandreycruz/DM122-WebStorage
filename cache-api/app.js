0d7af6537052e6d46b4bfc59b8263f48e36dac67
Edy Segura <edysegura@gmail.com>
2022-10-08 10:59:26 -0300

    feat: implement fetch pokemon data

---
+++ cache-api/app.js
@@ -0,0 +1,17 @@
+async function fetchPokeData() {
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon/282");
+  const pokeData = await response.json();
+  return pokeData;
+}
+
+async function showPokeData() {
+  const img = document.querySelector("img");
+  const details = document.querySelector("details");
+  const pre = document.querySelector("pre");
+  const pokeData = await fetchPokeData();
+  pre.textContent = JSON.stringify(pokeData, null, 2);
+  img.src = pokeData.sprites.other["official-artwork"].front_default;
+  img.alt = pokeData.name;
+  img.title = pokeData.name;
+  details.hidden = false;
+}
