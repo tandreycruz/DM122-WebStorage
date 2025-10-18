a2b2ab9f839ebb0c291ad6278062ee2da1a33df5
Taíbe Cruz <107949509+tandreycruz@users.noreply.github.com>
2025-10-18 09:55:45 -0300

    Feat: save to local storage

--- localstorage/app.js
+++ localstorage/app.js
@@ -3,4 +3,7 @@ const form = document.querySelector("form");
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     console.log("[app.js] form trigged!");
+    console.log("email: ", form.email.value);
+    window.localStorage.setItem('email', form.email.value);
+    console.log("[app.js] email saved!");
 })
