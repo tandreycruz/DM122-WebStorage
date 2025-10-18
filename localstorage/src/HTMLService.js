854df0ca0c1fcc3f7c657cad9aaeddac22741732
Taíbe Cruz <107949509+tandreycruz@users.noreply.github.com>
2025-10-18 14:46:32 -0300

    feat: integrate to html table

--- localstorage/src/HTMLService.js
+++ localstorage/src/HTMLService.js
@@ -8,13 +8,29 @@ export default class HTMLService {
         const form = document.querySelector("form");
         form.addEventListener("submit", (event) => {
             event.preventDefault();
-            console.log("[app.js] form trigged!");
+            console.log("[HTMLService.js] form trigged!");
             this.saveEmail(form.email.value);
         });
     }
 
-    saveEmail(email) {
+    async saveEmail(email) {
         if (!email) return;
-        console.log(`👁️ [HTMLService.js] email ${email}`);
+        console.log(`👁️ [HTMLService.js] saving email ${email}`);
+        const newSubscriber = await this.subscriberService.saveEmail(email);        
+        this.addToTable(newSubscriber);
+    }
+
+    addToTable(subscriber) {
+        const table = document.querySelector("table");
+        if (!table || !subscriber) return;
+        const tbody = table.tBodies[0];
+        const row = tbody.insertRow();
+        const dateCell = row.insertCell();
+        const emailCell = row.insertCell();
+        const deleteCell = row.insertCell();
+        dateCell.textContent = subscriber.createdDate.toLocaleString("pt-BR");
+        emailCell.textContent = subscriber.email;
+        deleteCell.textContent = "🗑️";
+        table.hidden = false;
     }
 }
--- localstorage/src/SubscriberService.js
+++ localstorage/src/SubscriberService.js
@@ -1,9 +1,22 @@
 export default class SubscriberService {
+    db = [];
+
     constructor() {
         console.log(`👁️ [SubscriberService.js] initialized`);
     }
 
     async saveEmail(email) {
-        console.log(`👁️ [SubscriberService.js] ${email}`);
+        if (!email) {
+            console.error(`[SubscriberService.js] no email provided`);
+            return;
+        }
+        const newRecord = {
+            createdDate: new Date(),
+            email,
+        };
+        this.db.push(newRecord);        
+        console.log(`👁️ [SubscriberService.js] ${email} added`);
+        console.table(this.db);
+        return newRecord;
     }
 }
