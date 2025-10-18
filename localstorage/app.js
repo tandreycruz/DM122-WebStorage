b7f337eb572dad547474c9f3f7e172e37919640b
Taíbe Cruz <107949509+tandreycruz@users.noreply.github.com>
2025-10-18 10:10:19 -0300

    feat: implement save/load data in local storage

--- localstorage/app.js
+++ localstorage/app.js
@@ -1,4 +1,5 @@
 const form = document.querySelector("form");
+const main = document.querySelector("main");
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -6,4 +7,17 @@ form.addEventListener("submit", (event) => {
     console.log("email: ", form.email.value);
     window.localStorage.setItem('email', form.email.value);
     console.log("[app.js] email saved!");
-})
+    loadSavedData();
+});
+
+function loadSavedData() {
+    console.log("[app.js] fetching email");
+    const email = window.localStorage.getItem("email");
+    if (!email) {
+        console.error("[app.js] no email found!");
+        return;
+    }
+    console.log(`[app.js] ${email}`);
+}
+
+loadSavedData();
